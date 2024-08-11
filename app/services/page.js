659f770cf46b81f55011a80bc4b2090c6b2571dc
junaaid96 <info.junaidulislam@gmail.com>
2024-08-11 17:10:55 +0600

    updates v2.3

--- app/services/page.js
+++ app/services/page.js
@@ -1,18 +1,56 @@
+"use client";
+
 import getAllServices from "@/lib/getAllServices";
-import { retryFetch } from "@/lib/retryFetch";
+// import { retryFetch } from "@/lib/retryFetch";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export const metadata = {
-    title: "NexTechCare - All Services",
-    description: "NexTechCare - A seamless IT support experience",
-};
+// export const metadata = {
+//     title: "NexTechCare - All Services",
+//     description: "NexTechCare - A seamless IT support experience",
+// };
 
-export default async function Services() {
-    let services = [];
-    try {
-        services = await retryFetch(getAllServices);
-    } catch (error) {
-        console.error("Failed to fetch services:", error);
+// export default async function Services() {
+//     let services = [];
+//     try {
+//         services = await getAllServices();
+//     } catch (error) {
+//         console.error("Error fetching services", error);
+//     }
+export default function Services() {
+    const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        document.title = "NexTechCare - All Services";
+        const metaDescription = document.querySelector(
+            'meta[name="description"]'
+        );
+        if (metaDescription) {
+            metaDescription.content =
+                "NexTechCare - A seamless IT support experience";
+        }
+
+        async function fetchServices() {
+            try {
+                const servicesData = await getAllServices();
+                setServices(servicesData);
+            } catch (error) {
+                console.error("Error fetching services", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchServices();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <span className="loading loading-dots loading-lg"></span>
+            </div>
+        );
     }
 
     return (
